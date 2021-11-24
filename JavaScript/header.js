8b519aae0d9c4cc198675826c610069d32c65d07
Harsh <harshkamboj180@gmail.com>
2021-11-24 10:43:46 +0530

    passwd_Validation

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -18,3 +18,24 @@ function closeButton2(){
     var close2 = document.getElementById('modal-2');
       close2.style.display = "none" ;
 }
+
+
+
+function validation(){
+    var user = document.getElementById('email123').value;
+    var pass = document.getElementById('pass123').value;
+    if(user==" "){
+         
+         return false;
+    }
+    if(pass.length <6 || pass.length > 8 ){
+                 document.getElementById('123').innerHTML = " Password length must be grerater than 6 and less than 9";
+                 return false;
+    }
+    if(pass.length >=6 && pass.length <=8){
+        document.getElementById('123').innerHTML = "OK";
+    }
+
+    
+    
+   }
--- JavaScript/valid.js
+++
@@ -1,8 +0,0 @@
-function validation(){
-    var user = document.getElementById('exampleInputEmail1').value;
-    if(user==" "){
-         document.getElementById('email123').innerHTML = "Please fill the email field";
-         return false;
-    } 
-    
-   }
